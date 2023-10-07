a0c64d0f6152cc3f1d4af31adf336ac2fa2dd62d
Attoukora <attoukoraange@gmail.com>
2023-10-07 20:08:31 +0000

    modification de la partie style

--- client/src/components/layout/Layout.js
+++ client/src/components/layout/Layout.js
@@ -1,14 +1,21 @@
 import React from "react";
 import { Navigation } from "../navigation/Navigation";
 import { Footer } from "../footer/Footer";
-import { Box, Container } from "@mui/material";
+import { Box, styled } from "@mui/material";
+
+const StyledBox = styled(Box)(({ theme }) => ({
+  width: "100%",
+  [theme.breakpoints.down("md")]: {
+    width: 600,
+  },
+}));
 
 export const Layout = ({ children }) => {
   return (
-    <Container maxWidth="xl">
+    <StyledBox>
       <Navigation />
       <Box sx={{ mt: 8 }}>{children}</Box>
       <Footer />
-    </Container>
+    </StyledBox>
   );
 };
